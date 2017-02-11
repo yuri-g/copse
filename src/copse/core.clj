c99f5edc166001ae7f5ed1decbfab17935aba84e
Yurii Honcharenko <yurii@yaraku.com>
2017-02-11 12:39:10 +0900

    First version of dynamic routes loading

--- src/copse/core.clj
+++ src/copse/core.clj
@@ -1,10 +1,9 @@
-(ns copse.core
-  (:require [clojure.java.io :as io]
-            [clojure.inspector :as inspector]))
+(ns copse.core)
 
-(defn- load-routes [app]
-  (println "Loading routes from:")
-  (app/test-test))
+(defn- load-routes [app-ns]
+  (println (str "Loading routes from: " app-ns))
+  (let [ns (ns-publics app-ns)]
+    (((symbol "routes") ns))))
 
 (defn bootstrap [app-namespace]
   (load-routes app-namespace))
